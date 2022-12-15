73c215b3a25fcf1a0639861ad335d77c8b2e382a
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2022-12-15 15:20:56 +0700

    Update UnetLargeRefined_train.py

--- UnetLargeRefined_train.py
+++ UnetLargeRefined_train.py
@@ -23,7 +23,7 @@ warnings.filterwarnings("ignore")
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--exp_dir', type=str, default='./', help='Experiment path') #Change
-parser.add_argument('--exp', type=str, default='singlePeople_UnetLarge_30_11', help='Name of experiment')
+parser.add_argument('--exp', type=str, default='singlePeople_UnetLargeRefined_30_11', help='Name of experiment')
 parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate') 
 parser.add_argument('--batch_size', type=int, default=32, help='Batch size,32')
 parser.add_argument('--weightdecay', type=float, default=1e-3, help='weight decay')
