352f57eff0254c603b0ba454499e34c2cd281840
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2022-12-22 13:23:45 +0700

    Update L2Ave.py

--- L2Ave.py
+++ L2Ave.py
@@ -35,3 +35,22 @@ with open('./predictions/L2/singlePeople_UnetLarge_30_11_dis_cp50.p', 'rb') as f
   print("Feet: ", (full[15] + full[16] + full[18] + full[19])/4)
   
   print()
+
+with open('./predictions/L2/singlePeople_UnetLarge_30_11_dis_cp82_22_12.p', 'rb') as f:
+  dis = pickle.load(f)
+  print("Large 82 cp: ")
+  print(type(dis), dis.shape)
+  full = np.mean(np.abs(dis), axis = 0)
+  print(full)
+  print("Ave: ", np.mean(full, axis = 0))
+  print("Head: ", full[0])
+  print("Shoulder: ", (full[2] + full[5])/2)
+  print("Elbow: ", (full[3] + full[6])/2)
+  print("Wrist: ", (full[4] + full[7])/2)
+  print("Hip: ", full[8])
+  print("Knee: ", (full[10] + full[13])/2)
+  print("Ankle: ", (full[11] + full[14])/2)
+  print("Feet: ", (full[15] + full[16] + full[18] + full[19])/4)
+  
+  print()
+  
